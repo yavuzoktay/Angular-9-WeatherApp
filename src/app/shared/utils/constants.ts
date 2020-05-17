6ba59435ba04baf0bb5b4398333da1276a18c4cd
Yavuz Oktay <yavuz.oktay@ibtech.com.tr>
2020-05-17 20:22:10 +0300

    .gitignore file update

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+  static readonly apiKey: string = 'e54146d561bbee2aeb5f6673578a761c';
+  static readonly host = 'https://api.openweathermap.org/data/2.5/onecall?';
+  static readonly exclude = '&exclude=current,minutely,hourly';
+}
